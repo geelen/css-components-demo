bb05ad03a5cf357046350e0aa2cee5eb597a7e2d
Glen Maddern <glenmaddern@gmail.com>
2016-08-03 13:01:16 +1000

    ok got the basic stuff working, now to port css

--- src/App.js
+++ src/App.js
@@ -7,34 +7,23 @@ import { backgrounds, typography } from './styles'
 import Nav from './Nav'
 import TweetDisplay from './TweetDisplay'
 
-const A = elem(
-  rules.backgroundColor('blue'),
-  nested('> .what',
-    rules.backgroundColor('red'),
-  )
-)
-const B = elem(
-  rules.backgroundColor('blue'),
-  nested('> .what',
-    rules.backgroundColor('blue'),
-  )
-)
 const App = elem(
   minHeight(vh(100)),
   backgrounds.white
 )
-//
-// const Main = elem('main', css`
-//   ${typography('sans')}
-//   max-width: 400px;
-//   margin: 0 auto;
-//   ${media('min-width: 600px)', css`
-//     max-width: 600px;
-//   `)}
-// `)
+const Main = elem('main', css`
+  ${typography('sans')}
+  max-width: 400px;
+  margin: 0 auto;
+  ${media('min-width: 600px)', css`
+    max-width: 600px;
+  `)}
+`)
 
 export default () =>
   <App>
-    <A/>
-    <B/>
+    <Nav/>
+    <Main>
+      <TweetDisplay/>
+    </Main>
   </App>
--- src/styled-elem/models/Root.js
+++ src/styled-elem/models/Root.js
@@ -1,9 +1,24 @@
 import concat from "../constructors/concat"
 import {hashObject} from 'aphrodite/lib/util'
+import {css as aphroditeInjectCss} from 'aphrodite/no-important'
+
+const joinSelectors = (outer, inner) => {
+  /* For the moment, just support & at the beginning */
+  return `${outer}${inner.startsWith('&') ? inner.slice(1) : ' ' + inner}`
+}
+
+/* Recursive CSS injector */
+const injectCss = (_name, _definition, _fragments) => {
+  const className = aphroditeInjectCss({ _name, _definition })
+  _fragments.forEach(({ selector, rules, fragments }) => {
+    injectCss(joinSelectors(_name, selector), rules, fragments)
+  })
+  return className
+}
 
 /*
-* The root node of a styling tree.
-* */
+ * The root node of a styling tree.
+ * */
 export default class Root {
   constructor(...rules) {
     this.ruleSet = concat(...rules)
@@ -11,8 +26,8 @@ export default class Root {
 
   /* This is aphrodite-specifc but could be changed up */
   injectStyles() {
-    const {rules, fragments} = this.ruleSet.flatten()
-    const hash = hashObject({rules, fragments})
-    setTimeout(() => console.log(JSON.stringify({rules, fragments, hash}, null, 2)), 200)
+    const { rules, fragments } = this.ruleSet.flatten()
+    const hash = '_' + hashObject({ rules, fragments })
+    return injectCss(hash, rules, fragments)
   }
 }
