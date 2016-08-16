3580379f439794b39ef1d6a2bf5a2781d950f4af
Glen Maddern <glenmaddern@gmail.com>
2016-08-16 18:03:58 +1000

    converted the first file to styled

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import { elem, rules, css, media, nested } from 'styled-components'
-import { minHeight } from 'styled-components/lib/rules'
-import { vh } from 'styled-components/lib/units'
+import styled, { elem, rules, css, media, nested } from 'styled-components'
 import { backgrounds, typography } from './styles'
 
 import Nav from './Nav'
 import TweetDisplay from './TweetDisplay'
 
-const App = elem(
-  minHeight(vh(100)),
-  backgrounds.blue,
-  nested('#root > &',
-    backgrounds.white
-  )
-)
-const Main = elem('main', css`
+const App = styled.div`
+  min-height: 100vh;
+  ${backgrounds.blue}
+  #root > & {
+    ${backgrounds.white}
+  }
+`
+
+const Main = styled.main`
   ${typography('sans')}
   max-width: 400px;
   margin: 0 auto;
   ${media('min-width: 600px', css`
     max-width: 600px;
   `)}
-`)
+`
 
 export default () =>
   <App>
